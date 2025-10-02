11646ab51dcb10bc5db721d686fad0887ac9725c
Danu Nasrulloh <nasrullahdanu501@gmail.com>
2025-10-02 23:17:44 +0700

    change text view Step ke lima Composable Function

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -8,13 +8,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Button as Buttom
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    Column (modifier = modifier.padding(16.dp)){
-    var count = 0
+    Column(modifier = modifier.padding(16.dp)) {
+        // Changes to count are now tracked by Compose
+        var count by remember { mutableStateOf(0) }
         Text("You've had $count glasses.")
-        Buttom(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)){
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
