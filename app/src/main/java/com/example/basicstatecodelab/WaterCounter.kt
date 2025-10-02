102e679f7988c046ecba092c4e975c7846fff9e5
Danu Nasrulloh <nasrullahdanu501@gmail.com>
2025-10-02 23:18:22 +0700

    change text view Step ke lima Composable Function

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,15 +1,12 @@
 package com.example.basicstatecodelab
 
-import android.R.attr.text
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Button as Buttom
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
