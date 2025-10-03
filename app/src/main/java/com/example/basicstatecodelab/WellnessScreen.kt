62dee01cac1e351f2f5a8d1baac473b3c75370cd
Danu Nasrulloh <nasrullahdanu501@gmail.com>
2025-10-03 13:47:10 +0700

    change text view Step ke 10 Work with list

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -2,12 +2,19 @@ package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-   Column(modifier= modifier) {
-       StatefulCounter()
-       WellnessTasksList()
-   }
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+    }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
