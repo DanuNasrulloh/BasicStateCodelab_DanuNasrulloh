94e4b00911911ec62140498d7721675372f06b4d
Danu Nasrulloh <nasrullahdanu501@gmail.com>
2025-10-03 11:03:24 +0700

    change text view Step ke 8 Restore state in Compose

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Button
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
 
@@ -17,7 +18,7 @@ import androidx.compose.runtime.setValue
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
 
         if (count > 0) {
             Text("You've had $count glasses.")
